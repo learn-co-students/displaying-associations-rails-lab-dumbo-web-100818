b30cc636513c2d97c2bc26ae331d2998f3c0f5ea
said shah <saidmysr20@aol.com>
2018-11-05 19:22:31 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
+require "pry"
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+
+  def song_count
+    @songs = Song.all.select{|song| song.artist == self}
+    @songs.length
+  end
+
+
 end
