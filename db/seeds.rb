5433ca20caa10485581d132a3312fa0e43e3b2aa
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-06 10:39:16 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ap = Artist.create(name: "Anderson Paak.")
+ap.songs.create!(title: "Miss Right")
+ap.songs.create!(title: "Suede")
+
+cole = Artist.create(name: "J. Cole")
+cole.songs.create!(title: "Apparently")
+cole.songs.create!(title: "Lights Please")
+cole.songs.create!(title: "Lost Ones")
